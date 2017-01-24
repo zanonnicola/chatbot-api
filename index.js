fa70326d76301784a0c05a9f872895216d9eca76
Nicola Zanon <nicola.zanon86@gmail.com>
2017-01-24 19:32:55 +0000

    Fix module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('./server/server');
 var config = require('./server/config/config');
+var logger = require('./server/util/logger');
 
 app.listen(config.port);
 logger.log('listening on http://localhost:' + config.port);
